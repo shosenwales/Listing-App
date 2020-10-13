dfbd8a919863269fc98d426f26aa7fce36f61a5b
shuaib <wales149@gmail.com>
2020-10-13 19:00:26 +0100

    fixed some some  issues

--- btre/settings.py
+++ btre/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gdhfjm$!hfnenng'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['immense-beyond-20828.herokuapp.com', '127.0.0.1', 'localhost']
 
